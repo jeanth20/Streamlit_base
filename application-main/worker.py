16782335f4b1258dc58adcaf6610ecc427fe4571
Jean <64726220+jeanth20@users.noreply.github.com>
2023-07-14 11:37:57 +0200

    Update worker.py

--- application-main/worker.py
+++ application-main/worker.py
@@ -20,3 +20,23 @@ def three():
 def four():
     st.title(":flag-za: Four function")
 
+def contact_form():
+    st.title("Contact form")
+    left_column, right_column = st.columns(2)
+    
+    with left_column:
+        name_first = st.text_input('First name')
+        name_last = st.text_input('Last name')
+        id_number = st.text_input('ID number')
+
+    with right_column:
+        gender = st.radio('Gender', ['Female','Male'])
+        email = st.text_input('Email address')
+        phone = st.text_input('Phone number')
+    
+
+    st.text_area('Text to translate')
+    st.camera_input("Take a picture")
+
+    st.checkbox('I agree')    
+    st.button('Submit')
